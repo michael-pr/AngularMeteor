86ae2cae7920b8c24a444561cc7283e2c26b9e8b
Michael Price <mtnp55@live.com>
2015-10-11 23:39:14 -0400

    Finished part 3

---
+++ app.js
@@ -0,0 +1,54 @@
+Parties = new Mongo.Collection("parties");
+
+if (Meteor.isClient) {
+  angular.module("socially", ["angular-meteor"]);
+
+  angular.module("socially").controller("PartiesListCtrl", ["$scope", "$meteor",
+    function ($scope, $meteor) {
+
+      $scope.parties = $meteor.collection(Parties);
+
+      /*$scope.parties = [
+        {
+          name: "Dubstep-Free Zone",
+          description: "Can we please just for an evening not listen to dubstep."
+        },
+        {
+          name: "All dustep all the time",
+          description: "Get it on!"
+        },
+        {
+          name: "Savage Lounging",
+          description: "Leisure suit requred. And only fiercest manngers."
+        }
+      ];*/
+    }
+  ]);
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Parties.find().count() === 0) {
+      var parties =  [
+        {
+          name: "Dubstep-Free Zone",
+          description: "Can we please just for an evening not listen to dubstep."
+        },
+        {
+          name: "All dustep all the time",
+          description: "Get it on!"
+        },
+        {
+          name: "Savage Lounging",
+          description: "Leisure suit requred. And only fiercest manngers."
+        }
+      ];
+
+      console.log("Adding parties");
+
+      for (var i = 0; i < parties.length; i++) {
+        Parties.insert(parties[i]);
+      }
+    }
+  });
+}
